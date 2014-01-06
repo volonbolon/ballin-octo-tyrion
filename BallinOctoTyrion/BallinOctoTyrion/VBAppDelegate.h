1aac964c0028594bccb8bfd18b256f4239faa0c3
Ariel Rodriguez <ariel@volonbolon.net>
2014-01-06 19:56:45 -0300

    Add Xcode project

---
+++ BallinOctoTyrion/BallinOctoTyrion/VBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VBAppDelegate.h
+//  BallinOctoTyrion
+//
+//  Created by Ariel Rodriguez on 1/6/14.
+//  Copyright (c) 2014 Ariel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BallinOctoTyrion/BallinOctoTyrion/VBViewController.h
@@ -0,0 +1,13 @@
+//
+//  VBViewController.h
+//  BallinOctoTyrion
+//
+//  Created by Ariel Rodriguez on 1/6/14.
+//  Copyright (c) 2014 Ariel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBViewController : UIViewController
+
+@end
